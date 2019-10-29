3f97d1373428373b813abc59be548618ecb556d7
Matt Cole <matt.s.cole@outlook.com>
2019-10-29 07:49:55 -0400

    fixed error in vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -10,7 +10,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_subnetwork" "default" {
       name          = var.subnetworks_name
       ip_cidr_range = var.subnetworks_cidr
-      network       = "${google_compute_network.default.self_link}" 
+      network       = "${google_compute_network.vpc_network.self_link}" 
       secondary_ip_range {
           range_name = var.secondary_name
           ip_cidr_range = var.secondary_cidr
