8f6e320e3b052e1b76545a4aa79a448da5be4424
Matt Cole <matt.s.cole@outlook.com>
2019-10-31 14:19:09 -0400

    added functionality to VPC network module

--- vpc/main.tf
+++ vpc/main.tf
@@ -30,7 +30,7 @@ resource "google_compute_subnetwork" "subnetwork" {
      
       name          = var.subnetworks_name
       ip_cidr_range = var.subnetworks_cidr
-      network       = google_compute_network.vpc_network.self_link
+      network       = local.network_self_link
       secondary_ip_range {
           range_name    = var.secondary_name
           ip_cidr_range = var.secondary_cidr
