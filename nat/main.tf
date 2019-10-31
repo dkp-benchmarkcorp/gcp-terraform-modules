c32e45680dce34f174c80e64cdb37267f19fe18f
Matt Cole <matt.s.cole@outlook.com>
2019-10-31 14:09:19 -0400

    added functionality to VPC network module

--- nat/main.tf
+++ nat/main.tf
@@ -1,16 +1,16 @@
 resource "google_compute_router" "router"{
-    name    = "${var.router_name}"
-    network = "${var.vpc_network}"
+    name    = var.router_name
+    network = var.network_name
 }
 
 resource "google_compute_router_nat" "nat" {
-    name                               = "${var.nat_name}"
-    router                             = "${google_compute_router.router.name}"
+    name                               = var.nat_name
+    router                             = google_compute_router.router.name
     nat_ip_allocate_option             = "AUTO_ONLY"
     source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
     log_config {
-      enable = "${var.nat_logging}"
+      enable = var.nat_logging
       filter = "ERRORS_ONLY"
     }
 }
