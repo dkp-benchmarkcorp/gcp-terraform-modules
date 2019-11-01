70e65ef4f26a888af39a065598ea055afd3eae12
Matt Cole <matt.s.cole@outlook.com>
2019-11-01 09:44:58 -0400

    added firewall module

--- firewall/variables.tf
+++ firewall/variables.tf
@@ -3,3 +3,9 @@ variable "firewall" {
   description = "List of firewall rules"
   default     = []
 }
+
+variable "port" {
+  type        = list(map(list))
+  description = "List of firewall rules"
+  default     = []
+}
