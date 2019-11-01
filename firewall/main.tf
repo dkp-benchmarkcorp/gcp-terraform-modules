4f05ca4c3877ed02e42acc3122a7032c279353a2
Matt Cole <matt.s.cole@outlook.com>
2019-11-01 12:31:09 -0400

    added firewall module

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,10 +1,12 @@
 resource "google_compute_firewall" "default" {
-  count = length(var.firewall)
-  name    = lookup(var.firewall[count.index], "name", "")
-  network = lookup(var.firewall[count.index], "network", "")
+  for_each = lookup(var.firewall, "name", "")
+  name    = each.value
+  for_each = lookup(var.firewall, "network", "")
+  network = each.value
 
   allow {
-    protocol = lookup(var.firewall[count.index], "protocol", "")
+    for_each = lookup(var.firewall, "protocol", "")
+    protocol = each.value
     for_each = lookup(var.firewall, "ports", "")
     ports    = each.value
   }
