53baa3a38d9361cb91ce0213e670beeacc65467b
Matt Cole <matt.s.cole@outlook.com>
2019-11-01 09:10:34 -0400

    added firewall module

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,11 +1,11 @@
 resource "google_compute_firewall" "default" {
   count = length(var.firewall)
-  name    = lookup(var.firewall[count.index])
+  name    = lookup(var.firewall[count.index], "name", ""))
   network = module.vpc.network_name
 
   allow {
-    protocol = lookup(var.firewall[count.index])
-    ports    = lookup(var.firewall[count.index])
+    protocol = lookup(var.firewall[count.index]), "protocol", "")
+    ports    = lookup(var.firewall[count.index]), "ports", "")
   }
 
   source_tags = compact(split(",", lookup(var.firewall[count.index], "tags", "")))
