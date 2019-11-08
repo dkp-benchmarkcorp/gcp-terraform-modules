8e80b90026489041a7df8a23eee7f09c9d55271f
Matt Cole <matt.s.cole@outlook.com>
2019-11-08 10:05:37 -0500

    added stuff

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,11 +1,12 @@
 resource "google_compute_firewall" "default" {
- count = length(var.firewall)
- name    = lookup(var.firewall[count.index], "name", "")
- network = lookup(var.firewall[count.index], "network", "")
+ count      = length(var.firewall)
+ name       = lookup(var.firewall[count.index], "name", "")
+ network    = lookup(var.firewall[count.index], "network", "")
+ 
  allow {
    protocol = lookup(var.firewall[count.index], "protocol", "")
    ports    = [lookup(var.firewall[count.index], "ports", "")]
  }
  source_ranges = [lookup(var.firewall[count.index], "source_ranges", "")]
- target_tags = [lookup(var.firewall[count.index], "target_tags", "")]
+ target_tags   = [lookup(var.firewall[count.index], "target_tags", "")]
 }
