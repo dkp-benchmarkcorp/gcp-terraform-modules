33858ce23d833bec76dcb023257a19063cb1c4a8
Matt Cole <matt.s.cole@outlook.com>
2019-12-10 08:07:00 -0500

    added static IP module

---
+++ compute-static/main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "default" {
+  count        = var.amount
+  name         = "${var.name_prefix}-${count.index+1}"
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = [var.tags]
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnetwork
+    access_config {
+      nat_ip = var.static-ip
+    }
+  }
+metadata = {
+    startup-script = "${file("${path.root}/scripts/${var.startup-script}")}"
+}
+}
