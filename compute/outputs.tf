6e9114b38178060a4d8b13daa5ee7c00b1f6013a
om-benchmarkcorp <66264524+om-benchmarkcorp@users.noreply.github.com>
2020-06-12 16:31:46 -0400

    Update and rename output.tf to outputs.tf
    Rough outputs.tf file for the compute module. Last output was added by Matt in a separate branch and carried over.

---
+++ compute/outputs.tf
@@ -0,0 +1,15 @@
+output "name" {
+    value = google_compute_instance.default.*.name
+    description = "The names of the instance being created"
+}
+output "instance_zone" {
+    value = google_compute_instance.default.*.zone
+    description = "The zone where the instance will be created"
+}
+output "subnet_name" {
+    value = google_compute_instance.default[*].network_interface.subnetwork
+    description = "The names of the subnetwork the instances will be located"
+}
+output "instance_ip_addr" {
+  value = google_compute_instance.default[*].network_interface.0.access_config.0.nat_ip
+} 
