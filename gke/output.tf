ca157d47c0998219c0aac15c06dc6c872b3d048b
Jonathan Sammut <59364094+js-benchmarkcorp@users.noreply.github.com>
2020-12-18 15:31:58 -0500

    Update output.tf

--- gke/output.tf
+++ gke/output.tf
@@ -1,14 +1,15 @@
-output "cluster_name" {
-  value       = google_container_cluster.primary.name
-  description = "Name of the GKE Cluster."
+
+output "cluster_names"{
+    value = google_container_cluster.primary.name
+    description = "The name of the gke cluster"
 }
 
-output "location" {
-  value       = google_container_cluster.primary.location
-  description = "Region the Cluster is created in GKE Cluster."
+output "cluster_locations"{
+    value = google_container_cluster.primary.location
+    description = "The regions or zones in which the cluster will be created"
 }
 
-output "project" {
-  value       = google_container_cluster.primary.project
-  description = "The Project that contains the GKE Cluster."
+output "project_name"{
+    value = google_container_cluster.primary.project
+    description = "The name of the project tht contains the cluster"
 }
