9ac6c2bd16e17d67792efe11766d49e036f1c4a5
js-benchmarkcorp <jon.sammut@benchmarkcorp.com>
2020-12-21 08:18:42 -0500

    commented out outputs for test

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -1,14 +1,14 @@
-output "cluster_names"{
-    value = google_container_cluster.primary.name
-    description = "The name of the gke cluster"
-}
+# output "cluster_names"{
+#     value = google_container_cluster.primary.name
+#     description = "The name of the gke cluster"
+# }
 
-output "cluster_locations"{
-    value = google_container_cluster.primary.location
-    description = "The regions or zones in which the cluster will be created"
-}
+# output "cluster_locations"{
+#     value = google_container_cluster.primary.location
+#     description = "The regions or zones in which the cluster will be created"
+# }
 
-output "project_name"{
-    value = google_container_cluster.primary.project
-    description = "The name of the project tht contains the cluster"
-}
+# output "project_name"{
+#     value = google_container_cluster.primary.project
+#     description = "The name of the project tht contains the cluster"
+# }
