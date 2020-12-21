c5ed71fd1d0da04615e248e7cb0cb7728e67da97
js-benchmarkcorp <jon.sammut@benchmarkcorp.com>
2020-12-21 16:12:12 -0500

    updated gke module

--- gke/main.tf
+++ gke/main.tf
@@ -1,17 +1,22 @@
-provider "google" {
-}
+
 resource "google_container_cluster" "primary" {
-  count             = length(var.cluster)
-  name              = lookup(var.cluster[count.index], "name", "")
-  project           = lookup(var.cluster[count.index], "project", "")
-  description       = lookup(var.cluster[count.index], "description", "")
-  location          = lookup(var.cluster[count.index], "location", "")
-  network           = lookup(var.cluster[count.index], "network", "")
-  subnetwork           = lookup(var.cluster[count.index], "subnetwork", "")
+  count                    = length(var.cluster)
+  name                     = lookup(var.cluster[count.index], "name", "")
+  project                  = lookup(var.cluster[count.index], "project", "")
+  description              = lookup(var.cluster[count.index], "description", "")
+  location                 = lookup(var.cluster[count.index], "location", "")
+  network                  = lookup(var.cluster[count.index], "network", "")
+  subnetwork               = lookup(var.cluster[count.index], "subnetwork", "")
   remove_default_node_pool = lookup(var.cluster[count.index], "remove_default_node_pool", "")
-  initial_node_count = lookup(var.cluster[count.index], "initial_node_count", "")
+  #A node pool should always be created with this modul no need to create any more defualt nodes then necessary.
+  initial_node_count        = 1
   default_max_pods_per_node = lookup(var.cluster[count.index], "default_max_pods_per_node", "")
-  
+
+  network_policy {
+    enabled  = lookup(var.cluster[count.index], "network_policy", "")
+    provider = lookup(var.cluster[count.index], "network_policy_provider", "")
+  }
+
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block   = lookup(var.cluster[count.index], "cidr_block", "")
@@ -24,16 +29,16 @@ resource "google_container_cluster" "primary" {
     enable_private_nodes    = lookup(var.cluster[count.index], "enable_private_nodes", "")
     master_ipv4_cidr_block  = lookup(var.cluster[count.index], "master_ipv4_cidr_block", "")
   }
- 
+
   master_auth {
     username = ""
     password = ""
 
     client_certificate_config {
-      issue_client_certificate = false
+      issue_client_certificate = lookup(var.cluster[count.index], "issue_client_certificate", "")
     }
   }
- 
+
   addons_config {
     http_load_balancing {
       disabled = lookup(var.cluster[count.index], "http_load_balancing", "")
@@ -41,10 +46,6 @@ resource "google_container_cluster" "primary" {
 
     horizontal_pod_autoscaling {
       disabled = lookup(var.cluster[count.index], "horizontal_pod_autoscaling", "")
-    }
-
-    network_policy_config {
-      disabled = lookup(var.cluster[count.index], "network_policy_config", "")
     }
   }
 
@@ -55,16 +56,24 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  count    = length(var.node_pools)
+  count      = length(var.node_pools)
   name       = var.node_pools[count.index]["name"]
   location   = lookup(var.node_pools[count.index], "location", "")
-  cluster    = google_container_cluster.primary[count.index].name
+  cluster    = google_container_cluster.primary[0].name
   node_count = lookup(var.node_pools[count.index], "node_count", "")
-  
+
+  autoscaling {
+    # Minimum number of nodes in the NodePool. Must be >=0 and <= max_node_count.
+    min_node_count = lookup(var.node_pools[count.index], "autoscaling_min_node_count", 2)
+
+    # Maximum number of nodes in the NodePool. Must be >= min_node_count.
+    max_node_count = lookup(var.node_pools[count.index], "autoscaling_max_node_count", 3)
+  }
+
   node_config {
     preemptible  = true
     machine_type = lookup(var.node_pools[count.index], "machine_type", "")
-    image_type = lookup(var.node_pools[count.index], "image_type", "")
+    image_type   = lookup(var.node_pools[count.index], "image_type", "")
 
     metadata = {
       disable-legacy-endpoints = "true"
@@ -73,7 +82,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
-      "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
 }
